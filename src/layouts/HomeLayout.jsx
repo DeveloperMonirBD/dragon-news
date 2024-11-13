dd5bfa94a9f2a0427ed222f3c36f16715ef842f8
Monir <mrmonir0558@gmail.com>
2024-11-13 23:18:28 +0600

    project setup and  header section done

---
+++ src/layouts/HomeLayout.jsx
@@ -0,0 +1,20 @@
+import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <header>
+                <Header />
+                <section>
+                    <LatestNews />
+                </section>
+            </header>
+
+            <nav></nav>
+            <main></main>
+        </div>
+    );
+};
+
+export default HomeLayout;
